5856004d9c328a8725f22ee285b9363c67d1d3c7
Baasilrazriz <baasilrazriz@gmail.com>
2025-04-18 01:19:07 +0500

    added health check api

---
+++ TapDoc-Mobile-App-Backend/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TapDoc_Mobile_App_Backend.Controllers
+{
+    [ApiController]
+    [Route("/Health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("check-server-health")]
+        public IActionResult CheckServerHealth()
+        {
+            return Ok(new { message = "Server is healthy", status = "Healthy" });
+        }
+    }
+}
